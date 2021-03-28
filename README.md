1b6a94977912625448a8e2ee52b1521cebe05451
Mab-Rock <ayman.benadra16@gmail.com>
2021-03-28 01:01:29 +0100

    Added twitter, facebook and linkedin share options

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# modern-quote-generator
+# Modern Quote Generator (modern-quote-generator)
 A modern quote generator using JavaScript ES6 and RESTful API
+
+## Navigation
+- [Modern Quote Generator (modern-quote-generator)](#modern-quote-generator-modern-quote-generator)
+  - [Navigation](#navigation)
+  - [Files and Directories](#files-and-directories)
+
+## Files and Directories
+I used a flat style which is to put everything in the root for the ease of usage and access and modification of the code base, and also separating the javascript into multiple files for Modularity of the build.
+
+|index.html
+|style.css
+|script.js
+|quotes.js
+|background.js
+
+so feel free to use the project as you want after cloning it from this repo.
--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-// Getting the Quote from Type.fit
-let apiQuotes = [];
+import { backgroundPatterns } from "./background-patterns.js";
 
+// Getting elements from the DOM
+const body = document.body;
+const quoteText = document.querySelector("#quote");
+const quoteAuthor = document.querySelector("#author");
+const twitterBtn = document.querySelector("#twitter");
+const facebookBtn = document.querySelector("#facebook");
+const linkedinBtn = document.querySelector("#linkedin");
+const newQuoteBtn = document.querySelector("#new-quote");
+
+// Helper function to get a random int value to be used as index later.
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+// Getting the Quote from Type.fit
+let apiQuotes = [];
+
 const newQuote = () => {
-  const randomIndex = getRandomInt(apiQuotes.length);
+  let randomIndex = getRandomInt(apiQuotes.length);
   const quote = apiQuotes[randomIndex];
   // console.log(quote);
+
+  // Updating the Quote Text and Author
+  if (quote.text.length > 100) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+  quoteAuthor.textContent = quote.author ? quote.author : "Anonymous";
+
+  // Randomly changing the background color.
+  randomIndex = getRandomInt(backgroundPatterns.length);
+  console.log(body.style.backgroundImage);
+  body.style.backgroundImage =
+    backgroundPatterns[randomIndex]["background image"];
+  console.log(body.style.backgroundImage);
 };
 
 const getQuote = async () => {
@@ -33,6 +61,44 @@ const getQuote = async () => {
     console.log("data :>> ", apiQuotes);
   }
 };
+
+// Tweet the quote!
+const tweetQuote = () => {
+  const quote = quoteText.textContent;
+  const author = quoteAuthor.textContent;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} ~${author}`;
+
+  window.open(twitterUrl, "_blank");
+};
+
+// Share the quote on Facebook!
+const facebookShare = () => {
+  const quote = quoteText.textContent;
+  const author = quoteAuthor.textContent;
+  const title = "Modern Quote Generator";
+  const personalLink = "benadra.me/modern-quote-generator";
+  const facebookUrl = `http://www.facebook.com/sharer.php?s=100&p[title]=${title}&p[url]=${encodeURIComponent(
+    personalLink
+  )}&p[quote]=${quote} ~${author}`;
+
+  window.open(facebookUrl, "_blank");
+};
+
+// Share the quote on Linkedin!
+const linkedinShare = () => {
+  const personalLink = "benadra.me/modern-quote-generator";
+  const linkedinUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(
+    personalLink
+  )}`;
+
+  window.open(linkedinUrl, "_blank");
+};
+
+// Event listeners for the buttons.
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+facebookBtn.addEventListener("click", facebookShare);
+linkedinBtn.addEventListener("click", linkedinShare);
 
 // On Load
 getQuote();
